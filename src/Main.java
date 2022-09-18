c1c7170fc76082a5beaaf4eaa7487de2bdd161d0
Daniel Eugénio <82546839+dannythe21st@users.noreply.github.com>
2022-09-18 10:54:09 +0100

    Main 1st comm

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_NAME_BY_PHONE  = "GN";
+    public static final String SAME_PHONE = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,8 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String NO_MATCH = "Phone number does not exist.";
+    public static final String ALL_DIFFERENT = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +56,9 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GET_NAME_BY_PHONE:
+                    getNameByPhone(in, cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +153,14 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void getNameByPhone(Scanner in, ContactBook cBook) {
+        int phone = in.nextInt();
+        String name = cBook.getNameByPhone(phone);
+        if (name == null)
+            System.out.println(NO_MATCH);
+        else
+            System.out.println();
     }
 }
